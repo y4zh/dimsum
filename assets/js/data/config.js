abe828d40e0b638db59b8c6a0af25a794bc6c467
y4zh <134795112+y4zh@users.noreply.github.com>
2026-04-07 04:44:51 +0700

    Add route colors for various transport lines

--- assets/js/data/config.js
+++ assets/js/data/config.js
@@ -1,1 +1,73 @@
+window.routeColors = {
+    // Mikrotrans
+    "JAK": "#00b0ec",
 
+    // N-BRT / Feeder
+    "4F": "#b900e2",
+    "7P": "#911d3c",
+    "11Q": "#10c0ff",
+    "11P": "#B2A5A3", //start rute rusun sampe 4E
+    "2F": "#B2A5A3", 
+    "10B": "#B2A5A3", 
+    "11C": "#B2A5A3", 
+    "11M": "#B2A5A3", 
+    "11R": "#B2A5A3", 
+    "11B": "#B2A5A3", 
+    "4E": "#B2A5A3",
+
+    // BRT Transjakarta
+    "1": "#d02127",
+    "1B": "#2AA8A4",
+    "4": "#502d5f",
+    "4D": "#E58BBA",
+    "4K": "#9626b5",
+    "5": "#BC5827",
+    "5B": "#905B3A",
+    "5C": "#9cd2c6",
+    "5M": "#ff5400",
+    "5N": "#BC5827",
+    "6": "#2ca74a",
+    "6A": "#76C18A",
+    "6B": "#99C175",
+    "6H": "#75358C",
+    "6K": "#4A8F4D",
+    "6M": "#7B3669",
+    "7": "#e2275b",
+    "7D": "#53bbb9",
+    "7W": "#a5405a",
+    "7Q": "#c45438",
+    "9": "#43a09a",
+    "9A": "#8D9F3D",
+    "9C": "#3C9F68",
+    "9D": "#4DB748",
+    "9N": "#783d3f",
+    "10": "#8f1a1e",
+    "11": "#2F4FA2",
+    "11D": "#a7bae0",
+    "11W": "#2d55a0",
+    "13B": "#972489",
+    "13E": "#761C86",
+    "L13E": "#761C86",
+    "P11": "#A9C498",
+    "B11": "#D07C28",
+    "B21": "#3BB59C",
+    "B25": "#A9C498",
+    "D11": "#669043",
+    "2B": "#266e9a",
+    "TWM-K1": "#2F4FA2",
+
+    // KRL Train Lines
+    "A | Basoetta": "#262262",      // Lin Bandara Soetta
+    "B | Bogor": "#ec2329",       // Lin Bogor/Nambo
+    "C | Cikarang": "#26baed",       // Lin Cikarang
+    "T | Tangerang": "#c25f28",       // Lin Tangerang
+    "R | Rangkasbitung": "#99ca3e",       // Lin Rangkasbitung
+    "TP | Tanjung Priok": "#ef509a",      // Lin Tanjung Priok
+    "CB | Cibubur": "#21409a",      // Lin Cibubur
+    "BK | Bekasi": "#006838",      // Lin Bekasi (LRT)
+    "MRT": "#ca2047",    // MRT Lin Utara Selatan
+    "KRL": "#26baed",     // KRL
+    "LW | Walahar" : "#b6b7b7",
+    "LJ | Jatiluhur" : "#404040",
+    "Whoosh": "#7B0D1E"
+}
